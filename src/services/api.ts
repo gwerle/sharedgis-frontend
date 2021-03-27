99838cc117fdd706f796613d0c5e275639c22437
Gustavo Werle <gustavo.werle2@gmail.com>
2021-03-27 16:55:16 -0300

    deploy: prepare env variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3333'
-      : 'http://ambiente-producao.com',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
 });
 
